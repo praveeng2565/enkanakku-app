4c99735e208cfedc9f312aeba674e5b90671cac9
Praveen G <rajathesmart7208@gmail.com>
2026-07-18 17:20:37 +0530

    updated app name

--- android/app/src/main/kotlin/com/praveen/enkanakku_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/praveen/enkanakku_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.enkannaku_app
+package com.praveen.enkanakku_app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:enkannaku_app/main.dart';
+import 'package:enkanakku_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
